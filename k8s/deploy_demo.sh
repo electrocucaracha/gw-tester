82613df8094746afa5c2f50eb250912a2181fa47
Victor Morales <electrocucaracha@gmail.com>
2020-09-17 16:42:41 -0700

    Centralize IP information in ETDC

--- k8s/deploy_demo.sh
+++ k8s/deploy_demo.sh
@@ -18,6 +18,9 @@ fi
 multi_cni="${MULTI_CNI:-multus}"
 
 exit_trap() {
+    if [[ "${DEBUG:-true}" == "true" ]]; then
+        set +o xtrace
+    fi
     printf "CPU usage: "
     grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage " %"}'
     printf "Memory free(Kb):"
@@ -39,17 +42,16 @@ kubectl apply -f etcd.yml
 kubectl scale deployment lte-etcd --replicas=1
 kubectl rollout status deployment/lte-etcd --timeout=3m
 
-# Deploy SAE-GW helm charts
 if [ -n "${PKG_MGR:-}" ] && [ "${PKG_MGR:-}" == "helm" ]; then
-    helm install saegw "./${multi_cni}/charts/saegw/"
-    for chart in pgw sgw; do
-        kubectl rollout status "deployment/saegw-$chart"
+    for chart in saegw mme enb; do
+        helm install "$chart" "./${multi_cni}/charts/$chart/"
     done
+    kubectl rollout status deployment/saegw-pgw --timeout=3m
 else
-    for pod in pgw sgw; do
+    for pod in pgw sgw mme enb; do
         kubectl apply -f "${pod}_${multi_cni}.yml"
-        kubectl wait --for=condition=ready pod "$pod" --timeout=3m
     done
+    kubectl wait --for=condition=ready pods pgw --timeout=3m
 fi
 
 # Deploy Http server
@@ -64,55 +66,8 @@ fi
 export PGW_SGI_IP
 envsubst \$PGW_SGI_IP < "http-server_${multi_cni}.yml" | kubectl apply -f -
 
-# Deploy MME service
-if [ "$multi_cni" == "multus" ]; then
-    SGW_S11_IP=$(kubectl get pods -l=app.kubernetes.io/name=sgw \
-    -o jsonpath='{.items[0].metadata.annotations.k8s\.v1\.cni\.cncf\.io/networks-status}' \
-    | jq -r '.[] | select(.name=="lte-s11").ips[0]')
-    PGW_S5C_IP=$(kubectl get pods -l=app.kubernetes.io/name=pgw \
-    -o jsonpath='{.items[0].metadata.annotations.k8s\.v1\.cni\.cncf\.io/networks-status}' \
-    | jq -r '.[] | select(.name=="lte-s5c").ips[0]')
-elif [ "$multi_cni" == "danm" ]; then
-    SGW_S11_IP=$(kubectl get pods -l=app.kubernetes.io/name=sgw \
-    -o jsonpath='{range .items[0].status.podIPs[*]}{.ip}{"\n"}' \
-    | grep "172.22.0")
-    PGW_S5C_IP=$(kubectl get pods -l=app.kubernetes.io/name=pgw \
-    -o jsonpath='{range .items[0].status.podIPs[*]}{.ip}{"\n"}' \
-    | grep "172.25.1")
-fi
-if [ -n "${PKG_MGR:-}" ] && [ "${PKG_MGR:-}" == "helm" ]; then
-    helm install mme "./${multi_cni}/charts/mme" \
-    --set sgw.s11.ip="$SGW_S11_IP" \
-    --set pgw.s5c.ip="$PGW_S5C_IP"
-    kubectl rollout status deployment/mme
-else
-    export SGW_S11_IP PGW_S5C_IP
-    envsubst \$PGW_S5C_IP,\$SGW_S11_IP < "mme_${multi_cni}.yml" | kubectl apply -f -
-    kubectl wait --for=condition=ready pod mme --timeout=3m
-fi
-
-# Deploy eNB service
-if [ "$multi_cni" == "multus" ]; then
-    MME_S1C_IP=$(kubectl get pods -l=app.kubernetes.io/name=mme \
-    -o jsonpath='{.items[0].metadata.annotations.k8s\.v1\.cni\.cncf\.io/networks-status}' \
-    | jq -r '.[] | select(.name=="lte-s1c").ips[0]')
-elif [ "$multi_cni" == "danm" ]; then
-    MME_S1C_IP=$(kubectl get pods -l=app.kubernetes.io/name=mme \
-    -o jsonpath='{range .items[0].status.podIPs[*]}{.ip}{"\n"}' \
-    | grep "172.21.1")
-fi
-if [ -n "${PKG_MGR:-}" ] && [ "${PKG_MGR:-}" == "helm" ]; then
-    helm install enb "./${multi_cni}/charts/enb" \
-    --set mme.s1c.ip="$MME_S1C_IP"
-    kubectl rollout status deployment/enb
-else
-    export MME_S1C_IP
-    envsubst \$MME_S1C_IP < "enb_${multi_cni}.yml" | kubectl apply -f -
-    kubectl wait --for=condition=ready pod enb --timeout=3m
-fi
-
 # Deploy External client
-kubectl wait --for=condition=ready pod http-server
+kubectl wait --for=condition=ready pods --all --timeout=3m
 if [ "$multi_cni" == "multus" ]; then
     ENB_EUU_IP=$(kubectl get pods -l=app.kubernetes.io/name=enb \
     -o jsonpath='{.items[0].metadata.annotations.k8s\.v1\.cni\.cncf\.io/networks-status}' \
@@ -130,6 +85,6 @@ elif [ "$multi_cni" == "nsm" ]; then
 fi
 export ENB_EUU_IP HTTP_SERVER_SGI_IP
 envsubst \$ENB_EUU_IP,\$HTTP_SERVER_SGI_IP < "external-client_${multi_cni}.yml" | kubectl apply -f -
-kubectl wait --for=condition=ready pod external-client
+kubectl wait --for=condition=ready pod external-client --timeout=3m
 
 trap ERR
